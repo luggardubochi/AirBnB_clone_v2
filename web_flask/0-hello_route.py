9aee4ccdf18fbf354108ed94ba4f556bb0f6567f
felixokolo <pheelix.info@gmail.com>
2022-10-14 21:35:30 +0000

    README addition

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,9 +5,12 @@ from flask import Flask
 import os
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_world():
     """hello world testing"""
     return 'Hello HBNB!'
-os.environ['FLASK_APP']="./web_flask/0-hello_route.py"
+
+
+os.environ['FLASK_APP'] = "./web_flask/0-hello_route.py"
 os.system("flask run --host=0.0.0.0")
