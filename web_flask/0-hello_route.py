aa1c2ebe2c965f88765a0d16636881962565ed8d
felixokolo <pheelix.info@gmail.com>
2022-10-14 21:16:15 +0000

    correction

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,9 +2,12 @@
 """Flask package"""
 
 from flask import Flask
+import os
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_world():
     """hello world testing"""
     return 'Hello HBNB!'
+os.environ['FLASK_APP']="./web_flask/0-hello_route.py"
+os.system("flask run --host=0.0.0.0")
