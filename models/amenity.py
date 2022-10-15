11a21fab47d88fb254931b1460b9ecd8d755f8f5
felixokolo <pheelix.info@gmail.com>
2022-10-15 08:58:35 +0100

    addition

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+from models.base_model import Base
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """Amenity  class
     Attributes:
         name (str): Amenity  name
     """
 
-    name = ""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """User class init
