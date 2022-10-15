2fddc0e221d4a255128f1cdd50d126b287dd497c
felixokolo <pheelix.info@gmail.com>
2022-10-15 08:04:23 +0000

    correction

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,10 +11,10 @@ def hello_world():
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb/', strict_slashes=False)
 def disp_hbnb():
     """hello world testing"""
-    return 'BNB'
+    return 'HBNB'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
